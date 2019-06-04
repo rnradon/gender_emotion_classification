5be8453f8677a8614cc31ff77f65580da84e88c3
Rishabh Narang <radon@Rishabhs-MacBook-Pro.local>
2019-06-04 21:39:35 +0530

    started with utils.py

---
+++ utils.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# ----This code segment will be shifted to the file where we need to import our data----"
+IMDB_DATASET_PATH = '../data/imdb_crop/'
+file = IMDB_DATASET_PATH + 'gender_df.csv'
+# ------------------
+
+def import_data(file_path, dataset_name):
+    data = pd.read_csv(file, sep=',')
+    
+    if str(dataset_name) == "imdb":
+        x_train, x_test, y_train, y_test = train_test_split(data['image_path'], data['gender'], test_size=0.2, random_state=42, shuffle=False)
+    
+    return x_train, x_test, y_train, y_test
